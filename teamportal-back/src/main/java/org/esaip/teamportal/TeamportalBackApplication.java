5bd22925eb264686126e5f4f3d7b95c8ba6a7045
Mathias Coulomb <coulomb.mathias@gmail.com>
2026-03-13 16:07:13 +0100

    Ajout BackEnd

---
+++ teamportal-back/src/main/java/org/esaip/teamportal/TeamportalBackApplication.java
@@ -0,0 +1,13 @@
+package org.esaip.teamportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamportalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamportalBackApplication.class, args);
+	}
+
+}
---
+++ teamportal-back/src/test/java/org/esaip/teamportal/TeamportalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esaip.teamportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamportalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
