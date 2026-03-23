3e2d26b58745c5eaf30e66da78e7de991ef71114
John Snow <john.snow@email.com>
2026-03-23 17:07:45 +0100

    Missing componentModel on TileMapper

--- teamportal-back/src/main/java/org/esaip/teamportal/mapper/TileMapper.java
+++ teamportal-back/src/main/java/org/esaip/teamportal/mapper/TileMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TileMapper {
     TileDto toDto(Tile tile);
     List<TileDto> toDtoList(List<Tile> tiles);
